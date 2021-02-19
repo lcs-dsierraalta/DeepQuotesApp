89e14bf3bdc7442bd09480f9a9a5dfc5d31b4ef1
Diego Sierraalta <dsierraalta@lcs.on.ca>
2021-02-19 11:53:53 -0500

    Initial Commit

---
+++ DeepQuotesApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DeepQuotesApp
+//
+//  Created by Diego Sierraalta on 2021-02-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DeepQuotesApp/DeepQuotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepQuotesAppApp.swift
+//  DeepQuotesApp
+//
+//  Created by Diego Sierraalta on 2021-02-19.
+//
+
+import SwiftUI
+
+@main
+struct DeepQuotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
